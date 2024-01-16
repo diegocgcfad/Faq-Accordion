b1b6474e4cb136b3c97ca625065eac9a278c7b5a
diegocgcfad <diegocgcfad@hotmail.com>
2024-01-16 20:45:18 -0300

    Adjusts + js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,13 @@
-function showAnswer(idd){
+/**function showAnswer(idd){
     document.getElementById(idd).style.display = 'block';
     document.getElementById('image').src = 'assets/images/icon-minus.svg';
+}**/
+
+
+document.getElementById("question").addEventListener("click", test);
+
+function test(){
+    document.getElementById('answer').id = 'selected-answer';
 }
+
+
