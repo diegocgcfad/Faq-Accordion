39a21740770efa6adca59b5e050957bc529bcb35
diegocgcfad <diegocgcfad@hotmail.com>
2024-01-17 13:38:16 -0300

    js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,13 +1,26 @@
-/**function showAnswer(idd){
+/**function showAnswer(idd,img){
     document.getElementById(idd).style.display = 'block';
-    document.getElementById('image').src = 'assets/images/icon-minus.svg';
-}**/
+    document.getElementById(img).src = 'assets/images/icon-minus.svg';
+}
 
 
 document.getElementById("question").addEventListener("click", test);
 
 function test(){
     document.getElementById('answer').id = 'selected-answer';
-}
+}**/
+
+//Passo 1 - Pegar as perguntas e respostas
+const question = document.querySelectorAll('.question');
+const answer = document.querySelectorAll('.answer');
+
+//Passo 2 - Adicionar a classe selecionada no parágrafo quando clica no título
+question.forEach((questions) =>{
+    questions.addEventListener('click', () =>{
+        answer.forEach((answers) =>{
+            answers.classList.add('selected');
+        })
+    })
+})
 
 
